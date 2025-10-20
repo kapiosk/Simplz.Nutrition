99da2ea639ab844787181f3fb4b22f6755cb40f3
AZ <29824083+kapiosk@users.noreply.github.com>
2025-10-21 01:46:18 +0300

    Add SQLite vector store and configure embedding and chat clients

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.Extensions.AI;
 using Simplz.Nutrition.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<SqliteOptions>(builder.Configuration.GetSection("Sqlite"));
 builder.Services.Configure<OllamaOptions>(builder.Configuration.GetSection("Ollama"));
+builder.Services.AddSqliteVectorStore(_ => "Data Source=:memory:");
+
+builder.Services.AddScoped<IEmbeddingGenerator<string, Embedding<float>>>(sp =>
+{
+    var options = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<OllamaOptions>>().Value;
+    return new OllamaSharp.OllamaApiClient(new Uri(options.Endpoint), options.EmbeddingModel);
+});
+builder.Services.AddScoped<IChatClient>(sp =>
+{
+    var options = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<OllamaOptions>>().Value;
+    return new OllamaSharp.OllamaApiClient(new Uri(options.Endpoint), options.ChatModel);
+});
 
 var app = builder.Build();
 
