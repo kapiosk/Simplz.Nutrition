2fc5de70ea3b3266009d693df6f86b380cdc52f5
AZ <29824083+kapiosk@users.noreply.github.com>
2025-10-22 01:30:31 +0300

    Add initial migration and model snapshot for Food, FoodCategory, FoodNutrient, and Nutrient entities

---
+++ Migrations/20251021222905_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simplz.Nutrition.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Food",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    FoodCategoryId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Food", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodCategory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodNutrient",
+                columns: table => new
+                {
+                    FoodId = table.Column<long>(type: "INTEGER", nullable: false),
+                    NutrientId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Amount = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodNutrient", x => new { x.FoodId, x.NutrientId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nutrient",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    UnitName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutrient", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Food");
+
+            migrationBuilder.DropTable(
+                name: "FoodCategory");
+
+            migrationBuilder.DropTable(
+                name: "FoodNutrient");
+
+            migrationBuilder.DropTable(
+                name: "Nutrient");
+        }
+    }
+}
